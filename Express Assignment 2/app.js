ee070e94d3bcab53b9d65649141cb2ef699b933f
Andrei Karanko <andr.karanko@gmail.com>
2023-11-22 21:39:55 +0200

    Added static css from public folder, styling and assignment 2

--- Express Assignment 2/app.js
+++ Express Assignment 2/app.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const path = require('path');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-const bodyParser = require('body-parser');
+const mainRoutes = require('./routes/main');
+const userRoutes = require('./routes/users');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use(userRoutes);
+app.use(mainRoutes);
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
