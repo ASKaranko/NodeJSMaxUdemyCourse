9c514bb6072feaa7deea3af704804931832aa263
Andrei Karanko <andr.karanko@gmail.com>
2023-11-27 21:11:33 +0200

    Added ejs solution

--- Express/app.js
+++ Express/app.js
@@ -5,13 +5,10 @@ const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-// handlebars are not part of express, that is why we need to use engine method
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
