9b5814181a62a7d6d026ddbf6441be0ec1f877ec
Andrei Karanko <andr.karanko@gmail.com>
2023-11-30 23:01:38 +0200

    Refactoring app to MVC, added controllers and models

--- Express/routes/shop.js
+++ Express/routes/shop.js
@@ -1,19 +1,8 @@
 const express = require('express');
 const path = require('path');
-const rootDir = require('../utils/path');
+const productsController = require('../controllers/products');
 const router = express.Router();
 
-const adminData = require('./admin');
-
-router.get('/', (req, res, next) => {
-    res.render('shop', { 
-        prods: adminData.products, 
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProducts: adminData.products.length > 0,
-        activeShop: true,
-        productCSS: true,
-    });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
