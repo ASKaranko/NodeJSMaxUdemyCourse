02985f797325ea7b4469750761df4dd0f56f2168
Andrei Karanko <andr.karanko@gmail.com>
2023-12-15 22:15:50 +0200

    Started sequelize

--- Express/util/database.js
+++ Express/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node',
-    password: 'p9oqzjTeWQJpkw'
+const sequelize = new Sequelize('node', 'root', 'p9oqzjTeWQJpkw', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
