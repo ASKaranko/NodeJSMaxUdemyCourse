0e617ca03d83faf1286cb81197545c121e179e7e
Andrei Karanko <andr.karanko@gmail.com>
2024-03-06 23:38:05 +0200

    Implemented get status and updated status

--- RestAPIProject/controllers/auth.js
+++ RestAPIProject/controllers/auth.js
@@ -70,3 +70,49 @@ exports.login = async (req, res, next) => {
         next(error);
     }
 };
+
+exports.getStatus = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.userId);
+        if (!user) {
+            const error = new Error(
+                'A user was not found. Status is undefined'
+            );
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({
+            message: 'Status fetched.',
+            status: user.status
+        });
+    } catch (error) {
+        if (!error.statusCode) {
+            error.statusCode = 500;
+        }
+        next(error);
+    }
+};
+
+exports.updateStatus = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.userId);
+        if (!user) {
+            const error = new Error(
+                'A user was not found. Status is undefined'
+            );
+            error.statusCode = 404;
+            throw error;
+        }
+        user.status = req.body.status;
+        await user.save();
+        res.status(200).json({
+            message: 'Status Updated.',
+            status: user.status
+        });
+    } catch (error) {
+        if (!error.statusCode) {
+            error.statusCode = 500;
+        }
+        next(error);
+    }
+};
--- RestAPIProject/routes/auth.js
+++ RestAPIProject/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { body } = require('express-validator');
 const authController = require('../controllers/auth');
 const User = require('../models/user');
+const isAuth = require('../middleware/isAuth');
 
 const router = express.Router();
 
@@ -27,5 +28,7 @@ router.put(
 );
 
 router.post('/login', authController.login);
+router.get('/status', isAuth, authController.getStatus);
+router.put('/status', isAuth, authController.updateStatus);
 
 module.exports = router;
