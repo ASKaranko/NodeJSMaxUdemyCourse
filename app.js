fac657e559f806bfaa3ca13ca370de3ed9d36840
Andrei Karanko <andr.karanko@gmail.com>
2024-06-18 23:04:25 +0300

    Assertion, union types

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+"use strict";
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const button = document.querySelector('button');
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
 }
-console.log(add(1, 6));
-console.log(add('1', '6'));
+button === null || button === void 0 ? void 0 : button.addEventListener('click', () => {
+    const num1Value = num1 === null || num1 === void 0 ? void 0 : num1.value;
+    const num2Value = num2 === null || num2 === void 0 ? void 0 : num2.value;
+    const result = add(num1Value, num2Value);
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -1,5 +1,19 @@
-function add(num1: number, num2: number) {
-    return num1 + num2;
+const num1 = document.getElementById('num1') as HTMLInputElement;
+const num2 = document.getElementById('num2') as HTMLInputElement;
+const button = document.querySelector('button');
+
+function add(num1: number | string, num2: number | string) {
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    } else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
 }
 
-console.log(add(1, 6));
+button?.addEventListener('click', () => {
+    const num1Value = num1?.value;
+    const num2Value = num2?.value;
+    const result = add(num1Value, num2Value);
+    console.log(result);
+});
