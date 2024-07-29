8e1da4cc0a755a9199a20967d8e4cab8fd743c59
Andrei Karanko <andr.karanko@gmail.com>
2024-07-29 20:59:57 +0300

    deno start

--- app.js
+++ app.js
@@ -1,36 +1,10 @@
-"use strict";
-const num1 = document.getElementById('num1');
-const num2 = document.getElementById('num2');
-const button = document.querySelector('button');
-const numResults = [];
-const textResults = [];
-function add(num1, num2) {
-    if (typeof num1 === 'number' && typeof num2 === 'number') {
-        return num1 + num2;
-    }
-    else if (typeof num1 === 'string' && typeof num2 === 'string') {
-        return num1 + ' ' + num2;
-    }
-    return +num1 + +num2;
-}
-function printResult(resultObj) {
-    console.log(resultObj.val);
-}
-button === null || button === void 0 ? void 0 : button.addEventListener('click', () => {
-    const num1Value = num1 === null || num1 === void 0 ? void 0 : num1.value;
-    const num2Value = num2 === null || num2 === void 0 ? void 0 : num2.value;
-    const numResult = add(+num1Value, +num2Value);
-    numResults.push(numResult);
-    const stringResult = add(num1Value, num2Value);
-    textResults.push(stringResult);
-    printResult({ val: numResult, timestamp: new Date() });
-    console.log(numResults, textResults);
-});
-const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('It worked!');
-    }, 1000);
-});
-myPromise.then((result) => {
-    console.log(result.split('w'));
-});
+const fs = require('fs').promises;
+
+const text = 'This is a test - and it should be stored in a file';
+fs.writeFile('node-message.txt', text)
+    .then(() => {
+        console.log('File written successfully');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
--- app.ts
+++ app.ts
@@ -1,50 +1,8 @@
-const num1 = document.getElementById('num1') as HTMLInputElement;
-const num2 = document.getElementById('num2') as HTMLInputElement;
-const button = document.querySelector('button');
+const text = 'This is a test - and it should be stored in a file';
 
-const numResults: Array<number> = [];
-const textResults: Array<string> = [];
+const encoder = new TextEncoder();
+const data = encoder.encode(text);
 
-type NumOrString = number | string;
-type Result = { val: number; timestamp: Date };
-
-interface ResultObj {
-    val: number;
-    timestamp: Date;
-}
-
-function add(num1: NumOrString, num2: NumOrString) {
-    if (typeof num1 === 'number' && typeof num2 === 'number') {
-        return num1 + num2;
-    } else if (typeof num1 === 'string' && typeof num2 === 'string') {
-        return num1 + ' ' + num2;
-    }
-    return +num1 + +num2;
-}
-
-function printResult(resultObj: ResultObj) {
-    console.log(resultObj.val);
-    
-}
-
-button?.addEventListener('click', () => {
-    const num1Value = num1?.value;
-    const num2Value = num2?.value;
-    const numResult = add(+num1Value, +num2Value);
-    numResults.push(numResult as number);
-    const stringResult = add(num1Value, num2Value);
-    textResults.push(stringResult as string);
-    printResult({ val: numResult as number, timestamp: new Date() });
-    console.log(numResults, textResults);
-    
-});
-
-const myPromise = new Promise<string>((resolve, reject) => {
-    setTimeout(() => {
-        resolve('It worked!');
-    }, 1000);
-});
-
-myPromise.then((result) => {
-    console.log(result.split('w'));
+Deno.writeFile('message.txt', data).then(() => {
+    console.log('File written to ./message.txt');
 });
